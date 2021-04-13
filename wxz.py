3f8c82cb71005f41e9bf05cbe5e54ed302e7be32
V3QS <stasswita@interia.pl>
2021-04-13 18:30:34 +0200

    hangman game

---
+++ wxz.py
@@ -0,0 +1,39 @@
+import time
+
+print("Hello in Hangman game")
+
+time.sleep(1)
+
+print("Starting guessing...")
+time.sleep(0.5)
+
+word = "secret"
+
+guesses = ""
+
+turns = 10
+
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char)
+        else:
+            print("-")
+            failed += 1
+    if failed == 0:
+        print("You won !!!")
+        break
+
+    guess  = input("Give a word: ")
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+        print("Wrong")
+        print("You have " + str(turns) + " left")
+        if turns == 0:
+            print("You lose")
+
+
+
