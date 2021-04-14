d7d922e23efdbcc5d9b14f524783c9d8370afdc7
V3QS <stasswita@interia.pl>
2021-04-14 19:27:47 +0200

    timer

--- olq.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def get_random_password(lenght):
-    letters = string.ascii_lowercase
-    password = "".join(random.choice(letters) for i in range(lenght))
-    print(password)
-get_random_password(8)
-
