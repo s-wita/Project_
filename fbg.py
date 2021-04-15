bf5ad7b8fad1b76e0617a466bbd82e7fa1bc7e7b
Stanislaw-swita <79470677+Stanislaw-swita@users.noreply.github.com>
2021-04-15 17:27:38 +0200

    Delete fbg.py

--- fbg.py
+++
@@ -1,6 +0,0 @@
-email = input("Enter your Email: ").strip()
-
-usernam = email[:email.index('@')]
-domain = email[email.index('@') + 1:]
-
-print(f"Your username is {usernam} and your domain is {domain}")
